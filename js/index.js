198c628d040bbecb09c384dd29670f17befc628b
Sanel Hodzic <sanel_hodzic_tz_91@hotmail.com>
2019-03-18 13:37:38 +0100

    get book row context and get book row status functions

--- js/index.js
+++ js/index.js
@@ -7,13 +7,37 @@ OdinLibrary = (function () {
     });
 
     const LOCAL_STORAGE_BOOK_LIST_KEY = 'bookList';
+
     const writeToLocalStorage = function (bookList) {
         localStorage.setItem(LOCAL_STORAGE_BOOK_LIST_KEY, JSON.stringify(bookList));
     }
+
     const readFromLocalStorage = function () {
         return JSON.parse(localStorage.getItem(LOCAL_STORAGE_BOOK_LIST_KEY))
     }
 
-
+    const getBookReadStatus = function (bookReadStatus) {
+      switch (bookReadStatus.toString()) {
+        case BOOK_STATUS.READ :
+            return 'Read'
+        case BOOK_STATUS.READING :
+            return 'Reading'
+        case BOOK_STATUS.UNREAD:
+            return 'Unread'
+        default:
+            return 'j'
+      }
+    }
+    
+    const getBookRowContext = function (bookReadStatus) {
+        switch (bookReadStatus.toString()) {
+            case BOOK_STATUS.READ:
+                return 'table-success'
+            case BOOK_STATUS.READING:
+                return 'table-warning'
+            case BOOK_STATUS.UNREAD:
+                return 'table-danger'
+        }
+    }
 
 })()
