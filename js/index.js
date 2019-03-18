a4eaf4363bffcf7cee44c5698a77bb546cc6efbd
Ifeanyi Osinakayah Joseph <ifeanyiosinakayah15@gmail.com>
2019-03-18 13:20:16 +0100

    Added function for reading and writing booklist to localStorage

--- js/index.js
+++ js/index.js
@@ -1,8 +1,19 @@
 OdinLibrary = (function () {
-  const BOOK_STATUS = Object.freeze({
-    UNREAD: '1',
-    READ: '2',
-    READING: '3',
-  });
-  const LOCAL_STORAGE_BOOK_LIST_KEY = 'bookList'  
+
+    const BOOK_STATUS = Object.freeze({
+        UNREAD: '1',
+        READ: '2',
+        READING: '3',
+    });
+
+    const LOCAL_STORAGE_BOOK_LIST_KEY = 'bookList';
+    const writeToLocalStorage = function (bookList) {
+        localStorage.setItem(LOCAL_STORAGE_BOOK_LIST_KEY, JSON.stringify(bookList));
+    }
+    const readFromLocalStorage = function () {
+        return JSON.parse(localStorage.getItem(LOCAL_STORAGE_BOOK_LIST_KEY))
+    }
+
+
+
 })()
