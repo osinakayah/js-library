720881be589c0851d497da4ac4da181197bb6388
Sanel Hodzic <sanel_hodzic_tz_91@hotmail.com>
2019-03-18 13:16:55 +0100

    Odin library js function BOOK STATUS

---
+++ js/index.js
@@ -0,0 +1,8 @@
+OdinLibrary = (function () {
+  const BOOK_STATUS = Object.freeze({
+    UNREAD: '1',
+    READ: '2',
+    READING: '3',
+  });
+  const LOCAL_STORAGE_BOOK_LIST_KEY = 'bookList'  
+})()
